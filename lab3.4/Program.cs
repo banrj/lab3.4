4a5137927ca95d92280f96677e7697f1799beb83
banrj <eh.ehr@bk.ru>
2022-11-07 23:46:29 +0200

    lab3.4

---
+++ lab3.4/Program.cs
@@ -0,0 +1,21 @@
+﻿// 13 variant medium Bogomolov Danill
+
+double Answer = 0;
+for (double position = -Math.PI; position <= Math.PI / 2; position += 0.2)
+{
+    Console.WriteLine($"x= : {position:F2}");
+    if (position > 2.5)
+    {
+        Answer = position + 1;
+    }
+    else if (position <= 2.5 && position <= 0)
+    {
+        Answer = 1 - Math.Pow(position, 5);
+    }
+    else if (position < 0)
+    {
+        Answer = position + Math.Log(Math.Abs(Math.Sin(position)));
+    }
+    Console.WriteLine($"y Равен: {Answer:F2}" );
+}
+
